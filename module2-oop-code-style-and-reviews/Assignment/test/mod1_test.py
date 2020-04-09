b719b960eaed1209f9a78d6399e961e1f0bbd2db
dougscohen <dougscohen@gmail.com>
2020-04-09 13:45:01 -0400

    added pytest

--- module2-oop-code-style-and-reviews/Assignment/test/mod1_test.py
+++ module2-oop-code-style-and-reviews/Assignment/test/mod1_test.py
@@ -1,6 +1,4 @@
 
-# THIS IS NOT COMPLETE
-
 import unittest
 
 import pandas
@@ -9,14 +7,12 @@ from dc_lambdata.mod1 import full_state_name
 
 class TestMod1(unittest.TestCase):
 
-    # Individual test case
+    # Individual test case, name of fucntion needs to be "test_%"
     def test_name_conversion(self):
-        # self.assertEqual()
+        
         df = pandas.DataFrame({"abbrev": ["CT", "CO", "CA", "TX"]})
-        # full_df = full_state_name(df)
-        # print(full_df.head())
         self.assertEqual(df.columns.tolist(), ['abbrev'])
-        full_state_name(df)
+        df = full_state_name(df)
         self.assertEqual(df.columns.tolist(), ['abbrev', 'state_name'])
 
         # TODO: consider also testing the actual values (perhaps the first row)
