96cc25b16fa091dbc609a83132cec1e205806fb9
dougscohen <dougscohen@gmail.com>
2020-04-09 15:57:27 -0400

    tested mod2 function split_date

---
+++ module2-oop-code-style-and-reviews/Assignment/test/mod2_test.py
@@ -0,0 +1,27 @@
+
+import unittest
+
+import pandas as pd
+
+from dc_lambdata.mod2 import split_date
+
+class TestMod2(unittest.TestCase):
+
+    # Individual test case, name of fucntion needs to be "test_%"
+    def test_split_date(self):
+        '''test to confirm that a dataframe with one column "date" will be
+        converted to 4 columns ('date', 'year', 'month', 'day') using the
+        split_date() function.
+        '''
+        df = pd.DataFrame({"date": ["01/02/2018", "03/17/2004", "03/31/1990",
+                                    "10/24/1960"]})
+        self.assertEqual(df.columns.tolist(), ['date'])
+        full_df = split_date(df)
+        self.assertEqual(full_df.columns.tolist(), ['date', 'year', 'month',
+                                                    'day'])
+
+        # TODO: consider also testing the actual values (perhaps the first row)
+
+
+if __name__ == '__main__':
+    unittest.main()
